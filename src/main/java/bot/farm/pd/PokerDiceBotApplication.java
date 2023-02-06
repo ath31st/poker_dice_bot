f2ee3de43e2311b72a696723f8cb94a8b2b83244
Vladimir Olennikov <ath31st@yandex.ru>
2023-02-06 16:15:01 +0300

    start new project. bot for discord

---
+++ src/main/java/bot/farm/pd/PokerDiceBotApplication.java
@@ -0,0 +1,13 @@
+package bot.farm.pd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokerDiceBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PokerDiceBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/bot/farm/pd/PokerDiceBotApplicationTests.java
@@ -0,0 +1,13 @@
+package bot.farm.pd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokerDiceBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
